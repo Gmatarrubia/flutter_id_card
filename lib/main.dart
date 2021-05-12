8a7ada45a6bd42cbd7875f66f699bd8e85a0b5be
Gonzalo Matarrubia <g.matarrubia@gmail.com>
2021-05-13 00:51:39 +0200

    Aplicacion completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,111 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() {
-  runApp(
-    MaterialApp(
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: HexColor("#48a3ca"),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              CircleAvatar(
+                radius: 80.0,
+                backgroundImage: AssetImage("images/avatar.jpg"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Gonzalo  Matarrubia\nGonzález',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'monoton',
+                  fontSize: 40.0,
+                  color: HexColor("#053856"),
+                ),
+              ),
+              Text("CEO & Co-funder",
+                style: TextStyle(
+                  fontFamily: 'ubuntu',
+                  fontSize: 30.0,
+                  color: HexColor("#053856"),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 50.0,
+                width: 380.0,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  border: Border.all(color: Colors.black, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+                    Icon(
+                      Icons.phone,
+                      color: HexColor("#36c874"),
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text("+34 649 816 396",
+                      style: TextStyle(
+                        fontFamily: 'ubuntu',
+                        fontSize: 25.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 50.0,
+                width: 380.0,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  border: Border.all(color: Colors.black, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+                    Icon(
+                      Icons.email,
+                      color: HexColor("#36c874"),
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text("g.matarrubia@asysnet.es",
+                      style: TextStyle(
+                        fontFamily: 'ubuntu',
+                        fontSize: 25.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,12 +40,12 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "mi_card_flutter");
+    gtk_header_bar_set_title(header_bar, "flutter_id_card");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else {
-    gtk_window_set_title(window, "mi_card_flutter");
+    gtk_window_set_title(window, "flutter_id_card");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
